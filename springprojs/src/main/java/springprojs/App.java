5ebd2e80a19a62393a02a9ea9bd8e1949f75f274
ibrahimcatakli <ibrahim.catakli@gmail.com>
2020-01-03 19:05:31 +0300

    commtis

---
+++ springprojs/src/main/java/springprojs/App.java
@@ -0,0 +1,13 @@
+package springprojs;
+
+/**
+ * Hello world!s
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
